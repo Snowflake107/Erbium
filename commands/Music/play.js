f54472944f8e2522fcf1df95b3ffda2d39957051
Ivy-0001 <69188068+Ivy-0001@users.noreply.github.com>
2021-05-04 13:28:53 +0400

    Add perms checker

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -16,7 +16,8 @@ class Play extends Command {
     async run(message, args) {
         if (!message.member.voice.channel) return message.reply("❌ | You are not in a voice channel!");
         if (message.guild.me.voice.channel && message.guild.me.voice.channelID !== message.member.voice.channelID) return message.reply("❌ | You are not in my voice channel!");
-        
+        if (!message.member.voice.channel.joinable) return message.reply("❌ | I don't have enough permission(s) to connect to your voice channel!");
+        if (!message.member.voice.channel.speakable) return message.reply("❌ | I don't have enough permission(s) to speak in your voice channel!");
         const query = args.join(" ");
         if (!query) return message.reply("❌ | Please include a search query!");
 
